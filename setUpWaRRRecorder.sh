990fa1bfe029ada46dfc9434f720aa088adb8cb8
Silviu ANDRICA <=>
2012-01-30 17:31:16 +0100

    Minor changes

--- setUpWaRRRecorder.sh
+++ setUpWaRRRecorder.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#svn co http://src.chromium.org/svn/trunk/tools/depot_tools
+svn co http://src.chromium.org/svn/trunk/tools/depot_tools
 export PATH="$PATH":`pwd`/depot_tools
-#gclient config http://src.chromium.org/svn/trunk/src 
-#gclient sync --revision src@71331
-cp -R WaRRRecorder/* src/third_party/WebKit/Source/WebCore/
+gclient config http://src.chromium.org/svn/trunk/src 
+gclient sync --revision src@71331
 cd src/build/
 svn update install-build-deps.sh 
 ./install-build-deps.sh
 cd ../../
+cp -R WaRRRecorder/* src/third_party/WebKit/Source/WebCore/
 gclient runhooks --force
 
 
